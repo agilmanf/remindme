9564246a4b35b4ac42aa234a55d7d8641b6ac443
agilmanf <agilmanf@gmail.com>
2022-06-07 13:32:21 +0700

    task 1 fix absolute size

--- src/sections/UserPaging.jsx
+++ src/sections/UserPaging.jsx
@@ -5,14 +5,12 @@ import "../styles/userPaging.css";
 function UserPaging() {
   return (
     <section id="user-paging">
-      <div className="wrapper">
-        <h2>
-          Download an app now and the various benefits you will get immediately
-        </h2>
-        <div className="btn-container">
-          <Button text="Download Now" variant="btn-white" />
-          <Button text="About App" custom="outline" />
-        </div>
+      <h2>
+        Download an app now and the various benefits you will get immediately
+      </h2>
+      <div className="btn-container">
+        <Button text="Download Now" variant="btn-white" />
+        <Button text="About App" custom="outline" />
       </div>
     </section>
   );
